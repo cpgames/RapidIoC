a3515d54aa4ba7220e6f98884c45c20f0d486e04
cpgames <chillpillgames@gmail.com>
2025-08-25 03:40:22 -0500

    Binding tests

---
+++ RapidIoCTests/Rapid_Binding_Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace cpGames.core.RapidIoC.Tests;
+
+[TestClass]
+public class Rapid_Binding_Tests
+{
+    [TestMethod]
+    public void Bind_And_Unbind_Test()
+    {
+        var bindOutcome = Rapid.Bind<TestView>(Rapid.RootKey);
+        Assert.IsTrue(bindOutcome);
+        var unbindOutcome = Rapid.Unbind<TestView>(Rapid.RootKey);
+        Assert.IsTrue(unbindOutcome);
+
+        bindOutcome = Rapid.Bind<TestView>(Rapid.RootKey);
+        Assert.IsTrue(bindOutcome);
+        unbindOutcome = Rapid.Unbind<TestView>(Rapid.RootKey);
+        Assert.IsTrue(unbindOutcome);
+    }
+}
